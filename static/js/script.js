54e6ce24485e41dd3f7a98a04b33230219e8dd2c
Shadyxstep <laumorgan.14@gmail.com>
2021-02-06 12:46:44 +0000

    added registration functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
-$(document).ready(function () {
-    $(".sidenav").sidenav({edge: "right"});
+/*
+    vanilla JavaScript for MaterializeCSS initialization
+*/
+
+document.addEventListener('DOMContentLoaded', function () {
+     let sidenavs = document.querySelectorAll(".sidenav");
+     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
 });
