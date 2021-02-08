57d8774cb0440a5bd94c294aa188e30366ddda69
Shadyxstep <laumorgan.14@gmail.com>
2021-02-08 13:32:05 +0000

    add accordion collapsible for recipes

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,8 @@
 */
 
 document.addEventListener('DOMContentLoaded', function () {
-     let sidenavs = document.querySelectorAll(".sidenav");
-     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+    let sidenavs = document.querySelectorAll(".sidenav");
+    let sidenavsInstance = M.Sidenav.init(sidenavs, { edge: "right" });
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
 });
