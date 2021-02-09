05180486ac97a7e7cf730ef719c9e2fdf9460141
Shadyxstep <laumorgan.14@gmail.com>
2021-02-09 11:22:51 +0000

    added more input fields on submit your own recipe page

--- static/js/script.js
+++ static/js/script.js
@@ -6,4 +6,12 @@ $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 1,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
