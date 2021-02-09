f396dcb565f3e964cdf7248c3c97ad5d898738db
Shadyxstep <laumorgan.14@gmail.com>
2021-02-09 12:12:56 +0000

    added recipe category dropdown

--- app.py
+++ app.py
@@ -99,7 +99,8 @@ def logout():
 
 @app.route("/add_recipe")
 def add_recipe():
-    return render_template("add_recipe.html")
+    categories = mongo.db.categories.find().sort("recipe_type", 1)
+    return render_template("add_recipe.html", categories=categories)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
