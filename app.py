5cd713c940aaed6029b4a8ce0a42d7abce94437e
Shadyxstep <laumorgan.14@gmail.com>
2021-04-09 18:19:11 +0000

    changed debug to false

--- app.py
+++ app.py
@@ -242,4 +242,4 @@ def beverage_categories():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
